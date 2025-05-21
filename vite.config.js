d4c5abc00411a5582c661ad7551bcd1dc1b70ec1
BayrolM <munozbayrol@gmail.com>
2025-05-21 15:46:28 -0500

    Subiendo el gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // Cambia esta línea
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/PAGREACTTTTT/', // Ajusta esto con el nombre de tu repositorio
+});
